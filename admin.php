7eae7d7a04ca0c235a82fdc0bccd4aba7c77d1b3
Alexey Zaplutakhin <zaplutakhin@gmail.com>
2018-10-31 09:59:10 +0300

    Исправлены ссылки

--- admin.php
+++ admin.php
@@ -8,7 +8,7 @@
  $tests = scandir($dir);
  foreach ($tests as $test){
      if ($test !=='..' and $test!=='.' and $test!=='test.php'){
-        $testname=json_decode(file_get_contents('http://university.netology.ru/user_data/azaplutakhin/tests/'.urlencode($test)), true);
+        $testname=json_decode(file_get_contents('tests'.urlencode($test)), true);
         $name=$testname['0']['testname'];
         echo "<p><a href='tests/test.php?test=$test'> $name </a></p>";
  }
--- test.php
+++ test.php
@@ -4,7 +4,7 @@ $dir = '.';
 $tests = scandir($dir);
 foreach ($tests as $test){
     if ($test !=='..' and $test!=='.' and $test!=='test.php'){
-       $testname=json_decode(file_get_contents('http://university.netology.ru/user_data/azaplutakhin/tests/'.urlencode($test)), true);
+       $testname=json_decode(file_get_contents('tests'.urlencode($test)), true);
        $name=$testname['0']['testname'];
        echo "<p><a href='test.php?test=$test'> $name </a></p>";
 }
